216cde8b95378e8fb957d751129710e2fdca26d0
antonindra2 <indraantonius11@gmail.com>
2020-11-30 22:00:07 +0700

    CreateWebAPI
    createWebAPI

---
+++ WebAPI/WebAPI/Models/Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Department
+    {
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            DeptId = id;
+            Name = name;
+          
+        }
+        
+        [Key]
+        public int DeptId { get; set; }
+        public string Name { get; set; }
+        
+
+    }
+}
---
+++ WebAPI/WebAPI/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+
+        }
+        public Employee(int id, string name,int age)
+        {
+            EmpId = id;
+            Name = name;
+            Age = age;
+        }
+        [Key]
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        [ForeignKey("department")]
+        public int DeptId { get; set; }
+
+        public Department department { get; set; }
+    }
+}
